6470e261186567b82ed4a7580fbf63cebec12887
tonyeandre <tonyeandre@gmail.com>
2025-05-21 20:39:14 -0500

    dev backend

--- dev/backend.tf
+++ dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "dev/terraform.state"
-    bucket         = "class38dominion-terraform-backend56"
+    bucket         = "pec-unique-s3-bucket1"
     region         = "us-east-2"
     dynamodb_table = "pec-fintech"
   }
